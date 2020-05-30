26a3be0cc39afeca355aedb8430a4503f70f6a87
hacker-sa <62072626+hacker-sa@users.noreply.github.com>
2020-05-30 18:29:45 -0300

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ import sys
 
 os.system("rm -rf /data/data/com.termux/files/home/TECLADO-SA")
 os.system("cd;cd T-SA;cp -rf TECLADO-SA /data/data/com.termux/files/home;cd;cd TECLADO-SA;toilet --g -f slant updated;python2 teclado-sa.py")
-os.system("cd;rm -rf T-SA")
+
